81112285b95802b4c54e342bd0971866d9975309
Vera <vera.berger815@gmail.com>
2025-12-01 22:56:46 -0500

    fof update

--- fof.py
+++ fof.py
@@ -30,7 +30,7 @@ time, Pos, mass, n_particles, BoxSize = read_gadget_snapshot(file_name)
 
 print("Scale factor: %g  (should be 1)" %(time))
 
-box_vol = BoxSize
+box_vol = BoxSize**3
 mean_dist = (box_vol/n_particles)**(1/3) # mean distance between particles
 print('mean distance between particles = {}'.format(mean_dist))
 
@@ -45,7 +45,6 @@ plt.hist2d(x, y, bins = 1000,  norm=mpl.colors.LogNorm())
 # FoF algorithm
 linking_length = mean_dist*0.2
 
-<<<<<<< HEAD
 # find distance between particle i and particle j
 def particle_distance(i, j, positions):
     x_i, y_i, z_i = positions[i, 0], positions[i, 1], positions[i, 2]
@@ -57,10 +56,5 @@ def particle_distance(i, j, positions):
 
 
 
-=======
-# pairwise_distances = cdist(Pos, Pos)
-pairwise_distances = fastdist.euclidean(Pos, Pos)
-print('done')
->>>>>>> b39b1ac53d50028d7f7d38300b1a99cc4fd421c3
 
 
